9b0f2ad4649cc0c80fb5b544bf2dae67bbfa85a4
tp <tp@octavore.com>
2018-06-26 21:28:12 -0700

    Set version for lightproxy explicitly.

--- lightproxy.rb
+++ lightproxy.rb
@@ -2,6 +2,7 @@ class Lightproxy < Formula
   desc "Use domain names for localhost services"
   homepage "https://github.com/octavore/lightproxy"
   url "https://github.com/octavore/lightproxy/releases/download/v1.0.1/lightproxy_1.0.1_darwin_amd64.tar.gz"
+  version "v1.0.1"
   sha256 "148c47310634fa424896e9fa6c6eeeb7b10aa174d7b44e22e19e5e7eed27cf64"
 
   bottle :unneeded
