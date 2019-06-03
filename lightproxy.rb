acf63f4e1d6764e399118c0be824c6f513df4733
tp <tp@octavore.com>
2019-06-03 15:01:29 -0700

    Bump lightproxy to v1.2.0.

--- lightproxy.rb
+++ lightproxy.rb
@@ -1,9 +1,9 @@
 class Lightproxy < Formula
   desc "Use domain names for localhost services"
   homepage "https://github.com/octavore/lightproxy"
-  url "https://github.com/octavore/lightproxy/releases/download/v1.1.1/lightproxy_1.1.1_darwin_amd64.tar.gz"
-  version "v1.1.1"
-  sha256 "a50223e1d1dc7fd3d3a5f55974c1bb01bfaf023c14c66181785216f8f29e4780"
+  url "https://github.com/octavore/lightproxy/releases/download/v1.2.0/lightproxy_1.2.0_darwin_amd64.tar.gz"
+  version "v1.2.0"
+  sha256 "b725ca2a078805349bdb013331ba8fed160025661f979392c490f4c1d4537519"
 
   bottle :unneeded
 
