339171579ca16c91fdc054a167da042afca14ac2
octavore <1707744+octavore@users.noreply.github.com>
2022-08-23 16:36:15 -0700

    Update delta.rb

--- delta.rb
+++ delta.rb
@@ -4,8 +4,6 @@ class Delta < Formula
   url "https://github.com/octavore/delta/releases/download/v0.6.1/delta-0.6.1.tar.gz"
   sha256 "f0f2ff1995a53579fe188c5e70a1a649926b5bca7026160f619e9d37b35f4a5f"
 
-  bottle :unneeded
-
   def install
     bin.install "delta"
   end
