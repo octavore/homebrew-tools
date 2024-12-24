d97b2e58b49a2ad9d6f421253e2e1d131be9fbb1
Avinash <avinash.kamath@scalekit.com>
2024-12-24 15:55:44 +0530

    Update lightproxy.rb
    plist is deprecated. Using service do to be cmaptible

--- lightproxy.rb
+++ lightproxy.rb
@@ -11,34 +11,15 @@ class Lightproxy < Formula
   end
 
   def post_install
-    system "lightproxy", "init"
+    system "#{bin}/lightproxy", "init"
   end
 
-  plist_options manual: "lightproxy"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>KeepAlive</key>
-          <true/>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/lightproxy</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true />
-          <key>StandardErrorPath</key>
-          <string>/dev/null</string>
-          <key>StandardOutPath</key>
-          <string>/dev/null</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_bin/"lightproxy"]
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
+    log_path var/"log/lightproxy.log"
+    error_log_path var/"log/lightproxy.log"
   end
 
   test do
