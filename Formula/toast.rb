7cfbbe3121f296ac61a45df55cc7da34d4cbb583
tp <tp@octavore.com>
2026-02-16 17:51:05 +0800

    Fix toast url.

--- Formula/toast.rb
+++ Formula/toast.rb
@@ -6,7 +6,7 @@ class Toast < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/octavore/toast/releases/download/v0.1.0/toast-v0.1.0.gz"
+      url "https://github.com/octavore/toast/releases/download/v0.1.0/toast-v0.1.0.tar.gz"
       sha256 "9aa382108e37e2ba24c2a1c8d82b43bf4299a9ddfc827aec8462a4ee8f77e629"
     elsif Hardware::CPU.intel?
       odie "toast is not supported on Intel macs"
